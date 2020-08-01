452303245489517ba3805c4e1f46e4fc5fc9c14b
Isabella <irodriguezcruz@ufl.edu>
2020-08-01 03:14:28 -0400

    Delete console.log

--- server/config/express.js
+++ server/config/express.js
@@ -11,7 +11,7 @@ module.exports.init = () => {
         connect to database
         - reference README for db uri
     */
-    mongoose.connect(process.env.DB_URI || require('./config').db.uri, {
+    mongoose.connect(process.env.DB_URI || require('./config.js').db.uri, {
         useNewUrlParser: true
     }).then((success) => console.log("Database connected successfully"))
     .catch((error) => console.log("error connecting to database", error));
