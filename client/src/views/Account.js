6350d670f6b85fb3ca0ba2c81a6b3a1a46274070
Cole <cole.fisher@ufl.edu>
2020-08-07 12:08:23 -0400

    Pins displayed, delete button added but not working

--- client/src/views/Account.js
+++ client/src/views/Account.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Account = () => {
-    
+    const [pins, setPins] = useState([]);
     const domain = `${window.location.origin === "http://localhost:3000" ? "http://localhost:5000" : window.location.origin}`;
 
     const token = localStorage.getItem("token");
@@ -17,16 +17,58 @@ const Account = () => {
             .then((res) => res.json())
             .then((data) => {
               console.log(data);
+              setPins(data);
             })
     }, []);
     
+    const onDeleteClick = (event) => {
+      event.preventDefault();
+      //Remove pin from db here
+    }
+    if (pins && pins.length > 0) {
+      const pinsArr = pins.map(pin => 
+          <div class="row mx-auto newsarticle">
+              <div class="col my-auto">
+                  <h3>Address</h3>
+                  <p>
+                  <b>Peaceful: </b>{pin.isViolent.toString()} <br/>
+                  <b>Latitude: </b>{pin.coordinates.lat} <br/>
+                  <b>Longitude: </b>{pin.coordinates.long}
+                  </p> 
+              
+                  <div className="delete-button bottom-corner">
+                    <i class="far fa-trash-alt" style={{marginRight: "5px"}}></i>
+                    <span onClick={onDeleteClick}>Delete</span>
+                  </div>
+              </div>
+          </div>
+      );
 
-    
-    return (
-        <div>
-            <h3> Welcome to the Account Page</h3>
-        </div>
-    )
+      return (
+          <div>
+              <div class="spacer"></div>
+              <div class="spacer"></div>
+              <div class="row"><h2 class="mx-auto protestlabel">Account</h2></div>
+              <div class="row">
+                <div class="col acct_col">
+                    <h3 class="acct_heading protestlabel">Protest History</h3>
+                    <div>{pinsArr}</div>
+                </div>
+                
+                <div class="col acct_col">
+                    <div><h3 class="acct_heading protestlabel">Comment History</h3></div> 
+                </div>
+                
+              </div>
+              
+          </div>
+      );
+  }
+  else {
+      return (
+          <div></div>
+      );
+  }
 }
 
 export default Account;
